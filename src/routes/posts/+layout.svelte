7040afbd5e86e3410bb1972015236e8fce50486c
Mckay Jensen <jensenm@uchicago.edu>
2023-06-09 14:50:59 -0500

    changed textcontent formatting

--- src/routes/posts/+layout.svelte
+++ src/routes/posts/+layout.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import Beautiful from "$lib/components/Beautiful.svelte";
-import ContactBar from "$lib/components/ContactBar.svelte";
-import NavBar from "$lib/components/NavBar.svelte";
+    import ContactBar from "$lib/components/ContactBar.svelte";
+    import NavBar from "$lib/components/NavBar.svelte";
     import TextContent from "$lib/components/TextContent.svelte";
 
 </script>
