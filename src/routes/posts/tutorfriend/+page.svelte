8f9db97227db793a2fd85d90e2aef929dfd6ab41
Mckay Jensen <jensenm@uchicago.edu>
2023-06-12 09:42:33 -0500

    added tutorfriend writeup

---
+++ src/routes/posts/tutorfriend/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+</script>
+
+<!-- DATE: 2023-06-12 -->
+<!-- TAGS: programming -->
+
+<h1>Using the OpenAI API for a math tutoring app</h1>
+
+<p>For anyone who hasn't been paying attention recently, OpenAI's latest LLMs have an impressive ability to explain solutions to questions, including programming and math problems. I wanted to explore using their API to create an application meant specifically for math training.</p>
+
+<p>There are two basic objectives here: first, I wanted an interface that can be used to create questions with mathematical syntax and that can be repeated with different variable values; second, I wanted students to be able to get an AI-generated explanation after attempting each problem. The idea is that teachers or tutors can create a set of questions for their students, which students can attempt and get feedback on, without teachers having to create detailed solutions for each problem.</p>
+
+<!-- ENDPREVIEW -->
+
+<p>I created a demo of this concept, which you can access <a href="https://tutorfriend.vercel.app">here</a>.</p>
