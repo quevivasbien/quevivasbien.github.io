fad16909b97ba67f685774d1e185cdaef0532ca3
quevivasbien <quevivasbien@gmail.com>
2024-06-26 21:39:11 -0400

    Updates

---
+++ _app/immutable/nodes/7.d8c8d162.js
@@ -0,0 +1,31 @@
+import{S as le,i as Be,s as Ce,k as _,q as R,l as b,m as g,r as S,h as a,n as w,b as A,G as i,u as ne,H as se,a as x,y as U,c as E,z as P,A as Q,T as ae,M as ie,g as X,d as j,B as J,Q as fe}from"../chunks/index.f5836231.js";function me(t){let u,r,l,o;return{c(){u=_("div"),r=R(t[0]),l=R(": "),o=R(t[1]),this.h()},l(n){u=b(n,"DIV",{class:!0});var B=g(u);r=S(B,t[0]),l=S(B,": "),o=S(B,t[1]),B.forEach(a),this.h()},h(){w(u,"class","text-center")},m(n,B){A(n,u,B),i(u,r),i(u,l),i(u,o)},p(n,[B]){B&1&&ne(r,n[0]),B&2&&ne(o,n[1])},i:se,o:se,d(n){n&&a(u)}}}function ce(t,u,r){let{title:l}=u,{option:o}=u;return t.$$set=n=>{"title"in n&&r(0,l=n.title),"option"in n&&r(1,o=n.option)},[l,o]}class K extends le{constructor(u){super(),Be(this,u,ce,me,Ce,{title:0,option:1})}}function pe(t){let u,r,l,o,n,B,M,C,d,k,m,N,c,z,p,V,h,L,T,D,Z,W,v,F,ee,te,$,G,ue,oe;return d=new K({props:{title:"Tense",option:t[2]}}),m=new K({props:{title:"Voice",option:t[3]}}),c=new K({props:{title:"Number",option:t[4]}}),p=new K({props:{title:"Gender",option:t[5]}}),h=new K({props:{title:"Case",option:t[6]}}),{c(){u=_("h1"),r=R("The Verbinator"),l=x(),o=_("div"),n=_("h2"),B=R(t[1]),M=x(),C=_("div"),U(d.$$.fragment),k=x(),U(m.$$.fragment),N=x(),U(c.$$.fragment),z=x(),U(p.$$.fragment),V=x(),U(h.$$.fragment),L=x(),T=_("div"),D=_("button"),Z=R("Randomize"),W=x(),v=_("div"),F=_("h3"),ee=R("Verb bank"),te=x(),$=_("textarea"),this.h()},l(e){u=b(e,"H1",{class:!0});var s=g(u);r=S(s,"The Verbinator"),s.forEach(a),l=E(e),o=b(e,"DIV",{class:!0});var I=g(o);n=b(I,"H2",{});var q=g(n);B=S(q,t[1]),q.forEach(a),M=E(I),C=b(I,"DIV",{});var f=g(C);P(d.$$.fragment,f),k=E(f),P(m.$$.fragment,f),N=E(f),P(c.$$.fragment,f),z=E(f),P(p.$$.fragment,f),V=E(f),P(h.$$.fragment,f),f.forEach(a),I.forEach(a),L=E(e),T=b(e,"DIV",{class:!0});var y=g(T);D=b(y,"BUTTON",{class:!0});var O=g(D);Z=S(O,"Randomize"),O.forEach(a),y.forEach(a),W=E(e),v=b(e,"DIV",{class:!0});var Y=g(v);F=b(Y,"H3",{class:!0});var re=g(F);ee=S(re,"Verb bank"),re.forEach(a),te=E(Y),$=b(Y,"TEXTAREA",{class:!0,rows:!0}),g($).forEach(a),Y.forEach(a),this.h()},h(){w(u,"class","text-4xl font-bold"),w(o,"class","max-w-xl mx-auto m-2 p-4 bg-white rounded-lg flex flex-col text-center space-y-4"),w(D,"class","text-white bg-orange-700 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg px-5 py-2.5 text-center max-w-xl"),w(T,"class","flex flex-col text-center max-w-xs mx-auto my-8"),w(F,"class","border-b-2 border-red-500 w-2/3"),w($,"class","w-2/3 p-2 border-x-2 border-b-2 border-red-100"),w($,"rows","10"),w(v,"class","max-w-lg mx-auto mt-16 py-8 bg-white rounded-lg flex flex-col text-center items-center")},m(e,s){A(e,u,s),i(u,r),A(e,l,s),A(e,o,s),i(o,n),i(n,B),i(o,M),i(o,C),Q(d,C,null),i(C,k),Q(m,C,null),i(C,N),Q(c,C,null),i(C,z),Q(p,C,null),i(C,V),Q(h,C,null),A(e,L,s),A(e,T,s),i(T,D),i(D,Z),A(e,W,s),A(e,v,s),i(v,F),i(F,ee),i(v,te),i(v,$),ae($,t[0]),G=!0,ue||(oe=[ie(D,"click",t[7]),ie($,"input",t[8])],ue=!0)},p(e,[s]){(!G||s&2)&&ne(B,e[1]);const I={};s&4&&(I.option=e[2]),d.$set(I);const q={};s&8&&(q.option=e[3]),m.$set(q);const f={};s&16&&(f.option=e[4]),c.$set(f);const y={};s&32&&(y.option=e[5]),p.$set(y);const O={};s&64&&(O.option=e[6]),h.$set(O),s&1&&ae($,e[0])},i(e){G||(X(d.$$.fragment,e),X(m.$$.fragment,e),X(c.$$.fragment,e),X(p.$$.fragment,e),X(h.$$.fragment,e),G=!0)},o(e){j(d.$$.fragment,e),j(m.$$.fragment,e),j(c.$$.fragment,e),j(p.$$.fragment,e),j(h.$$.fragment,e),G=!1},d(e){e&&a(u),e&&a(l),e&&a(o),J(d),J(m),J(c),J(p),J(h),e&&a(L),e&&a(T),e&&a(W),e&&a(v),ue=!1,fe(oe)}}}function H(t){return t[Math.floor(Math.random()*t.length)]}function de(t,u,r){let l=`λυω
+πεμπω
+παυω
+παιδευω
+κελευω
+θσω
+γραφω
+φυλαττω
+θαπτω
+εθελω
+διδασκω
+ταττω
+βλαπτω
+πραττω
+αρχω
+πειθω
+δουλευω
+πολιτευω
+χορευω
+κωλυω
+κλεπτω
+λειπω
+ηκω
+σωζω
+αγω
+αδικεω
+νικαω
+ποιεω
+τιμαω`,o=l.split(`
+`);const n=["present","future","aorist","perfect"],B=["active","middle","passive"],M=["singular","plural"],C=["masculine","feminine","neuter"],d=["nominative","genitive","dative","accusative"];let k,m,N,c,z,p;function V(){r(1,k=H(o)),r(2,m=H(n)),r(3,N=H(B)),r(4,c=H(M)),r(5,z=H(C)),r(6,p=H(d))}V();function h(){l=this.value,r(0,l)}return t.$$.update=()=>{t.$$.dirty&1&&(o=l.split(`
+`),V())},[l,k,m,N,c,z,p,V,h]}class _e extends le{constructor(u){super(),Be(this,u,de,pe,Ce,{})}}export{_e as component};
